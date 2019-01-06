69c28216afb44bcc92d0f8f1066d79e3cb4dc4ac
yutoKataoka <akoatak@gmail.com>
2019-01-06 22:56:17 +0900

    learn jsx

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello world.</h1>;
+    const greeting = "Hi, Tom!";
+    const dom = <h1 className="foo">{greeting}</h1>;
+    return (
+      <React.Fragment>
+       <label htmlFor="bar">bar</label>
+       <input type="text" onChange={() => {console.log("I am clicked!")}} />
+      </React.Fragment>
+    );
   }
 }
 
