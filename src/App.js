afe334bc67cac2bb837d368537d63f7da0c574ef
yutoKataoka <akoatak@gmail.com>
2019-01-06 23:21:44 +0900

    learn pros of Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
 
-// class App extends Component {
-//   render() {
-//     const greeting = "Hi, Tom!";
-//     const dom = <h1 className="foo">{greeting}</h1>;
-//     return (
-//       <React.Fragment>
-//        <label htmlFor="bar">bar</label>
-//        <input type="text" onChange={() => {console.log("I am clicked!")}} />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
 const App = () => {
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 5},
+    {name: "Noname"}
+  ]
+
   return (<div>
-    <Cat />
-    <Cat />
-    <Cat />
+    {profiles.map((profile, index) => {
+      return <User name={profile.name} age={profile.age} key={index}/>
+    })}
   </div>)
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
